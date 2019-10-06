dd3b4710de4d73f5caccffb44281d8166d22f717
Your Name <you@example.com>
2019-10-05 23:45:41 -0400

    depth working

--- tauv_vehicle/src/depth/depth_sensor.py
+++ tauv_vehicle/src/depth/depth_sensor.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import Temperature
 from std_msgs.msg import Float64
-from ms5837-python import ms5837
+from std_msgs.msg import Header
+from ms5837lib import ms5837
+from time import sleep
 
 pub = rospy.Publisher('depth', Float64, queue_size=10)
 pub = rospy.Publisher('temperature', Temperature, queue_size=10)
@@ -14,17 +17,27 @@ class depth_sensor():
       self.pub_depth = rospy.Publisher('depth', Float64, queue_size=10)
       self.pub_temp = rospy.Publisher('temperature', Temperature, queue_size=10)
       self.ms5837 = ms5837.MS5837_30BA()
+      while not self.ms5837.init():
+          rospy.sleep(10)
+          print("Failed to initialize depth sensor, retrying in 10 seconds")
+      print("Depth sensor initialized!")
 
    def start(self):
       r = rospy.Rate(10) # 10hz
       while not rospy.is_shutdown():
          self.ms5837.read()
          self.pub_depth.publish(Float64(self.ms5837.depth()))
-         self.pub_temp.publish(Temperature(self.ms5837.temperature(), 0))
+         tempmsg = Temperature()
+         tempmsg.header = Header()
+         tempmsg.header.stamp = rospy.Time.now()
+         tempmsg.temperature = self.ms5837.temperature()
+         self.pub_temp.publish(tempmsg)
          r.sleep()
 
-
-if __name__ == '__main__':
+def main():
    d = depth_sensor()
    rospy.init_node('depth_sensor')
    d.start()
+
+if __name__ == '__main__':
+    main()
