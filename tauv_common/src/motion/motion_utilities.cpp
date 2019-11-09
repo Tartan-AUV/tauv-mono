5d103f595d3d5d4b7b5de8ee204ab89cefc4300d
larry <larry.geng12@gmail.com>
2019-11-09 14:24:38 -0500

    change c++ version and move curly braces

--- tauv_common/src/motion/motion_utilities.cpp
+++ tauv_common/src/motion/motion_utilities.cpp
@@ -7,47 +7,55 @@
 #include "constructors/Twist.h"
 #include "constructors/Vector3.h"
 
-Mover::Mover(ros::NodeHandle &nh) {
+Mover::Mover(ros::NodeHandle &nh)
+{
     std::string mover_topic = "/tartan_sub/mover";
     vel_pub_ = nh.advertise<geometry_msgs::Twist>(mover_topic, 1);
 }
 
-void Mover::dive(float duration, float speed) {
+void Mover::dive(float duration, float speed)
+{
     geometry_msgs::Twist msg = 
         Twist(Vector3(0, 0, speed), Vector3(0, 0, 0));
     publishDuration(msg, duration);
 }
 
-void Mover::forward(float duration, float speed) {
+void Mover::forward(float duration, float speed)
+{
     geometry_msgs::Twist msg = 
         Twist(Vector3(speed, 0, 0), Vector3(0, 0, 0));
     publishDuration(msg, duration);
 }
 
-void Mover::strafe(float duration, float speed) {
+void Mover::strafe(float duration, float speed)
+{
     geometry_msgs::Twist msg = 
         Twist(Vector3(0, speed, 0), Vector3(0, 0, 0));
     publishDuration(msg, duration);
 }
 
-void Mover::turn(float duration, float speed) {
+void Mover::turn(float duration, float speed)
+{
     geometry_msgs::Twist msg = 
         Twist(Vector3(0, 0, 0), Vector3(0, 0, speed));
     publishDuration(msg, duration);
 }
 
-void Mover::commonEnd() {
+void Mover::commonEnd()
+{
     if (ros::ok())
         publishMessage(Twist(Vector3(0, 0, 0), Vector3(0, 0, 0)));
 }
 
-void Mover::publishDuration(geometry_msgs::Twist vel, float duration) {
+void Mover::publishDuration(geometry_msgs::Twist vel, float duration)
+{
     float end_time = (float)ros::Time::now().toSec() + duration;
     while (ros::Time::now().toSec() < end_time && ros::ok())
         publishMessage(vel);
     commonEnd();
 }
 
-void Mover::publishMessage(geometry_msgs::Twist vel) {
+void Mover::publishMessage(geometry_msgs::Twist vel)
+{
     vel_pub_.publish(vel);
 }
