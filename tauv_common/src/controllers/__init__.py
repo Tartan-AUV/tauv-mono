04b6000e5705ec8d5970f37d05adb59c989f795a
Toms42 <tomscherlis@gmail.com>
2019-11-09 16:48:34 -0500

    progress on control aggregator

--- tauv_common/src/controllers/__init__.py
+++
@@ -1,2 +0,0 @@
-import keyboard_controller
-import control_aggregator
--- tauv_common/src/controllers/control_aggregator.py
+++ tauv_common/src/controllers/control_aggregator.py
@@ -10,6 +10,20 @@ from std_msgs.msg import Header
 
 class ControlAggregator:
     def __init__(self):
-        self.output_frame = rospy.get_param('~/output_frame')
+        self.controllers = rospy.get_param('configs/controllers')
+        self.topic_suffix = rospy.get_param('configs/topic_suffix')
+        self.enable_suffix = rospy.get_param('enable_suffix')
+        self.output_frame = rospy.get_param('configs/output_frame')
 
     def start(self):
+        print(("controllers: {}\n"
+               "topic_suffix: {}\n"
+               "enable_suffix: {}\n"
+               "output_frame: {}").format(
+            self.controllers, self.topic_suffix, self.enable_suffix, self.output_frame))
+
+
+def main():
+    rospy.init_node('control_aggregator')
+    ca = ControlAggregator()
+    ca.start()
