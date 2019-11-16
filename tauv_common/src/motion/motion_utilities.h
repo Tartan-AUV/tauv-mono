a7ed4e1e550ead475e456324ad2f118b2b3ca3be
larry <larry.geng12@gmail.com>
2019-11-16 13:33:54 -0500

    basic/poor implementation of units

--- tauv_common/src/motion/motion_utilities.h
+++ tauv_common/src/motion/motion_utilities.h
@@ -5,28 +5,31 @@
 
 #include "units/units.h"
 
+using namespace units::time;
+using namespace units::velocity;
+
 class Mover {
     public:
         Mover(ros::NodeHandle &nh);
 
         // move in z direction
-        void dive(float duration, float speed);
+        void dive(second_t duration, meters_per_second_t speed);
 
         // move in x direction
-        void forward(float duration, float speed);
+        void forward(second_t duration, meters_per_second_t speed);
 
         // move in y direction
-        void strafe(float duration, float speed);
+        void strafe(second_t duration, meters_per_second_t speed);
 
         // angular turn in z direction
-        void turn(float duration, float speed);
+        void turn(second_t duration, meters_per_second_t speed);
 
     private:
         // stop sending messages
         void commonEnd();
 
         // publish message for duration
-        void publishDuration(geometry_msgs::Twist vel, float duration);
+        void publishDuration(geometry_msgs::Twist vel, second_t duration);
 
         // publish message
         void publishMessage(geometry_msgs::Twist vel);
