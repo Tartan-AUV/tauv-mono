e09b49794c320cc4f327f1ed7781445fa55ed4c5
larry <larry.geng12@gmail.com>
2019-11-16 13:33:54 -0500

    basic/poor implementation of units

--- tauv_common/src/motion/motion_utilities.cpp
+++ tauv_common/src/motion/motion_utilities.cpp
@@ -7,37 +7,42 @@
 #include "constructors/Twist.h"
 #include "constructors/Vector3.h"
 
+#include "units/units.h"
+
+using namespace units::time;
+using namespace units::velocity;
+
 Mover::Mover(ros::NodeHandle &nh)
 {
     std::string mover_topic = "/tartan_sub/mover";
     vel_pub_ = nh.advertise<geometry_msgs::Twist>(mover_topic, 1);
 }
 
-void Mover::dive(float duration, float speed)
+void Mover::dive(second_t duration, meters_per_second_t speed)
 {
     geometry_msgs::Twist msg =
-        Twist(Vector3(0, 0, speed), Vector3(0, 0, 0));
+        Twist(Vector3(0, 0, (float)speed), Vector3(0, 0, 0));
     publishDuration(msg, duration);
 }
 
-void Mover::forward(float duration, float speed)
+void Mover::forward(second_t duration, meters_per_second_t speed)
 {
     geometry_msgs::Twist msg =
-        Twist(Vector3(speed, 0, 0), Vector3(0, 0, 0));
+        Twist(Vector3((float)speed, 0, 0), Vector3(0, 0, 0));
     publishDuration(msg, duration);
 }
 
-void Mover::strafe(float duration, float speed)
+void Mover::strafe(second_t duration, meters_per_second_t speed)
 {
     geometry_msgs::Twist msg =
-        Twist(Vector3(0, speed, 0), Vector3(0, 0, 0));
+        Twist(Vector3(0, (float)speed, 0), Vector3(0, 0, 0));
     publishDuration(msg, duration);
 }
 
-void Mover::turn(float duration, float speed)
+void Mover::turn(second_t duration, meters_per_second_t speed)
 {
     geometry_msgs::Twist msg =
-        Twist(Vector3(0, 0, 0), Vector3(0, 0, speed));
+        Twist(Vector3(0, 0, 0), Vector3(0, 0, (float)speed));
     publishDuration(msg, duration);
 }
 
@@ -47,10 +52,10 @@ void Mover::commonEnd()
         publishMessage(Twist(Vector3(0, 0, 0), Vector3(0, 0, 0)));
 }
 
-void Mover::publishDuration(geometry_msgs::Twist vel, float duration)
+void Mover::publishDuration(geometry_msgs::Twist vel, second_t duration)
 {
-    float end_time = (float)ros::Time::now().toSec() + duration;
-    while (ros::Time::now().toSec() < end_time && ros::ok())
+    ros::Time end_time = ros::Time::now() + ros::Duration((float)duration);
+    while (ros::Time::now() < end_time && ros::ok())
         publishMessage(vel);
     commonEnd();
 }
