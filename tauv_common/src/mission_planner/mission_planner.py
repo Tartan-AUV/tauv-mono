0af77cee3d928ac2199bf23b2416e874c1b0b793
SirLegolot <ngong2020@gmail.com>
2020-03-21 15:12:46 -0400

    ininitial framwork for mission_planner

--- tauv_common/src/mission_planner/mission_planner.py
+++ tauv_common/src/mission_planner/mission_planner.py
@@ -1,12 +1,10 @@
-# state_estimation.py
+# mission_planner.py
 #
-# This node is the starting point for everything related to state estimation.
-# Currently an EKF runs from the ROS Robot Localization package.
-# This node publishes a Pose using the depth sensor data, which is incorporated into the EKF
-# In the future, any processing of Visual Odometry data, or other sensors will pass through here
+# Yuh.
 #
-#
-# Author: Advaith Sethuraman 2019
+# Author: Neel Gandhi & Rishabh Jain
+
+
 
 
 #!/usr/bin/env python
@@ -22,49 +20,75 @@ import tauv_msgs
 import numpy as np
 from tauv_msgs.msg import FluidDepth
 
+class Task:
+  def __init__(self, timeout):
+    self.timeout = timeout
+    self.status = "Not Started"
+    self.done = False
+    self.name = ""
+    # Declare publishers, subcribers, etc. 
 
-imu_data = Imu()
-depth_data = 0
+class Mission_Planner:
+  def __init__(self, task_list):
+    self.task_list = task_list
+    self.cur_task = None
+    self.pub = rospy.Publisher("mission_output", String, queue_size = 50)
 
-class Depth_Odom:
-    def __init__(self):
-        self.depth_data = 0.0
-        self.depth_data_variance = .1
-        self.depth_data_frame = ""
-        self.imu_data = Imu()
-        self.depth_sensor_sub = rospy.Subscriber("sensors/depth", FluidDepth, self.depth_data_callback)
-        self.imu_sensor_sub = rospy.Subscriber("sensors/imu/data", Imu, self.imu_data_callback)
-        self.depth_odom_pub = rospy.Publisher("sensors/depth_odom", Odometry, queue_size=50)
-        self.map_broadcaster = tf.TransformBroadcaster()
+  def run(self):
+    for task in self.task_list:
+      self.cur_task = task.name
+      start_time = rospy.get_time()
+      while (rospy.get_time() - start_time < task.timeout) and (not task.done):
+        continue
+            # do something
 
-    def depth_data_callback(self, data):
-        self.depth_data = data.depth
-        self.depth_data_frame = data.header.frame_id
-        self.depth_data_variance = data.variance
-
-    def imu_data_callback(self, data):
-        self.imu_data = data
+  def spin(self):
+    self.pub.publish(self.cur_task)
 
 
-    def spin(self):
-        odom = Odometry()
-        odom.header.stamp = rospy.Time.now()
-        odom.header.frame_id = "odom"
-        odom.child_frame_id = "pressure_link"
-        pose_msg = PoseWithCovarianceStamped()
-        odom.pose.pose.position.z = -self.depth_data
-        odom.pose.covariance[14] = self.depth_data_variance**2
-        self.depth_odom_pub.publish(odom)
-        # q = self.imu_data.orientation
-        # q = [q[0], q[1], q[2], q[3]]
-        # self.map_broadcaster.sendTransform((0, 0, 0),
-        #                                    Quaternion(*q),
-        #                                    rospy.Time.now(),
-        #                                    "albatross/imu_debug",
-        #                                     "albatross/odom")
+
+# imu_data = Imu()
+# depth_data = 0
+
+# class Depth_Odom:
+#     def __init__(self):
+#         self.depth_data = 0.0
+#         self.depth_data_variance = .1
+#         self.depth_data_frame = ""
+#         self.imu_data = Imu()
+#         self.depth_sensor_sub = rospy.Subscriber("sensors/depth", FluidDepth, self.depth_data_callback)
+#         self.imu_sensor_sub = rospy.Subscriber("sensors/imu/data", Imu, self.imu_data_callback)
+#         self.depth_odom_pub = rospy.Publisher("sensors/depth_odom", Odometry, queue_size=50)
+#         self.map_broadcaster = tf.TransformBroadcaster()
+
+#     def depth_data_callback(self, data):
+#         self.depth_data = data.depth
+#         self.depth_data_frame = data.header.frame_id
+#         self.depth_data_variance = data.variance
+
+#     def imu_data_callback(self, data):
+#         self.imu_data = data
+
+
+#     def spin(self):
+#         odom = Odometry()
+#         odom.header.stamp = rospy.Time.now()
+#         odom.header.frame_id = "odom"
+#         odom.child_frame_id = "pressure_link"
+#         pose_msg = PoseWithCovarianceStamped()
+#         odom.pose.pose.position.z = -self.depth_data
+#         odom.pose.covariance[14] = self.depth_data_variance**2
+#         self.depth_odom_pub.publish(odom)
+#         # q = self.imu_data.orientation
+#         # q = [q[0], q[1], q[2], q[3]]
+#         # self.map_broadcaster.sendTransform((0, 0, 0),
+#         #                                    Quaternion(*q),
+#         #                                    rospy.Time.now(),
+#         #                                    "albatross/imu_debug",
+#         #                                     "albatross/odom")
 
 def main():
-    rospy.init_node('state_estimation', anonymous=True)
+    rospy.init_node('mission_planner', anonymous=True)
     d_odom = Depth_Odom()
     while not rospy.is_shutdown():
         d_odom.spin()
