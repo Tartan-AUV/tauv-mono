f85f4e5e1470a1327d73fec1b93f952e66fd87a7
Advaith Sethuraman <advaiths@gmail.com>
2020-06-03 00:01:04 -0400

    added vision_msgs to dependencies

--- tauv_common/src/vision/detector_bucket/detector_bucket.py
+++ tauv_common/src/vision/detector_bucket/detector_bucket.py
@@ -8,45 +8,39 @@
 
 #!/usr/bin/env python
 import rospy
+import tf
+import tf_conversions
+import numpy as np
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import *
 from nav_msgs.msg import Odometry
-import tf_conversions
 from tf.transformations import *
-import tf
 from geometry_msgs.msg import Quaternion
 from tauv_msgs.msg import BucketDetection, BucketList
 from tauv_common.srv import RegisterObjectDetection
-import numpy as np
 
 
-#TODO: Add service, create class for client bucket access
 class Detector_Bucket():
     def __init__(self):
-        self.depth_odom_pub = rospy.Publisher("vision/bucket_list", BucketList, queue_size=50)
+        self.bucket_list_pub = rospy.Publisher("bucket_list", BucketList, queue_size=50)
         self.detection_server = rospy.Service("detector_bucket/register_object_detection", RegisterObjectDetection, self.register_object_detection)
-        self.refresh_rate = 0 #set this using params in future
+        self.refresh_rate = 0 
         self.detections = []
 
-    def valid_registration(self, bucket_detection):
+    def is_valid_registration(self, bucket_detection):
         return True
 
     def register_object_detection(self, req):
         bucket_detection = req.bucket_detection
         img = req.image
         bbox_2d = req.bbox_2d
-        if(self.valid_registration(bucket_detection)):
+        if(self.is_valid_registration(bucket_detection)):
             self.detections.append(bucket_detection)
             return True
         return False
 
     def spin(self):
-        #use frame_id in the header
-        d = BucketDetection()
-        d.info = "testing"
-        d2 = BucketDetection()
-        d2.info = "lol"
-        self.depth_odom_pub.publish([d, d2])
+        self.bucket_list_pub.publish(self.detections)
         return
 
 def main():
