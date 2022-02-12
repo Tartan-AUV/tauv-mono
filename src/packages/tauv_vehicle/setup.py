22b8a339d92afc71d1f392b51aecd136bd08b19d
Theo Chemel <theo@chemel.net>
2022-02-12 14:10:31 -0500

    Update setup.py

--- src/packages/tauv_vehicle/setup.py
+++ src/packages/tauv_vehicle/setup.py
@@ -20,11 +20,12 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['depth_sensor', 'thrusters', 'sonar', 'xsens_imu_sync', 'teledyne_dvl'],
+    packages=['depth_sensor', 'thrusters', 'battery', 'sonar', 'xsens_imu_sync', 'teledyne_dvl'],
     package_dir={'': 'src'},
     requires=['rospy'],
     scripts=['scripts/depth_sensor',
              'scripts/actuators',
+             'scripts/battery',
              'scripts/sonar',
              'scripts/xsens_imu_sync',
              'scripts/teledyne_dvl']
