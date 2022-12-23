5421fadb86b0f0fee065cbf4d3307875a5917961
Theo Chemel <theo@chemel.net>
2022-12-22 22:06:08 -0500

    More thruster work

--- src/packages/tauv_sim_gazebo_plugins/include/tauv_sim_gazebo_plugins/thruster_plugin.h
+++ src/packages/tauv_sim_gazebo_plugins/include/tauv_sim_gazebo_plugins/thruster_plugin.h
@@ -1,5 +1,8 @@
-
 #pragma once
+
+#include <ros/ros.h>
+#include <boost/scoped_ptr.hpp>
+#include <std_msgs/Float64.h>
 
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
@@ -13,9 +16,23 @@ class ThrusterPlugin : public ModelPlugin
   void Load(physics::ModelPtr model, sdf::ElementPtr sdf);
   void OnUpdate(const common::UpdateInfo &info);
 
-  protected:
+  private:
+  void HandleTargetThrust(const std_msgs::Float64::ConstPtr& target_thrust);
+
   physics::ModelPtr model;
   physics::LinkPtr link;
+  int thrusterID;
+
   event::ConnectionPtr updateConnection;
+
+  boost::scoped_ptr<ros::NodeHandle> rosNode;
+
+  ros::Subscriber subTargetThrust;
+  ros::Publisher pubThrust;
+
+  float targetThrust;
+
+  common::Time updatePeriod;
+  common::Time lastUpdateTime;
 };
 }
