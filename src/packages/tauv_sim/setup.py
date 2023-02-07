2791871e7cd2ebc987a24a2ee6a3004e69db2fc4
Theo Chemel <theo@chemel.net>
2023-02-06 23:06:47 -0500

    Blow away old tauv_sim package

--- src/packages/tauv_sim/setup.py
+++ src/packages/tauv_sim/setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['thrusters_sim', 'xsens_imu_sim', 'teledyne_dvl_sim', 'imu_dvl_transformer'],
+    packages=[],
     package_dir={'': 'src'},
     requires=['rospy'],
-    scripts=['scripts/thrusters_sim', 'scripts/xsens_imu_sim', 'scripts/teledyne_dvl_sim', 'scripts/imu_dvl_transformer']
+    scripts=[]
 )
 
 setup(**setup_args)
