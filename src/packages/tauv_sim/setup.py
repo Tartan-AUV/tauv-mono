519c339a45e0aad778bbdadc93ff6ba955d125f6
Gleb Ryabtsev <gl.ryabtsev1@gmail.com>
2023-02-28 22:44:03 -0500

    Disparity to depth node for simulating the oakds

--- src/packages/tauv_sim/setup.py
+++ src/packages/tauv_sim/setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=[],
+    packages=['cameras'],
     package_dir={'': 'src'},
     requires=['rospy'],
-    scripts=[]
+    scripts=['scripts/disparity_to_depth']
 )
 
 setup(**setup_args)
