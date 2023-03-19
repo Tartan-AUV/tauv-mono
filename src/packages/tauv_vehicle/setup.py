ef2686c184f6df6c67b204b7b1435a034ea556ae
Theo Chemel <theo@chemel.net>
2023-03-19 15:19:18 -0400

    Oakd updates for multi camera

--- src/packages/tauv_vehicle/setup.py
+++ src/packages/tauv_vehicle/setup.py
@@ -20,7 +20,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['depth_sensor', 'thrusters', 'battery', 'sonar', 'xsens_imu_sync', 'teledyne_dvl', 'arduino', 'cameras'],
+    packages=['depth_sensor', 'thrusters', 'battery', 'sonar', 'xsens_imu_sync', 'teledyne_dvl', 'arduino', 'oakd'],
     package_dir={'': 'src'},
     requires=['rospy'],
     scripts=['scripts/depth_sensor',
@@ -30,7 +30,7 @@ setup_args = generate_distutils_setup(
              'scripts/xsens_imu_sync',
              'scripts/teledyne_dvl',
              'scripts/arduino',
-             'scripts/oakd_ros']
+             'scripts/oakd']
 )
 
 setup(**setup_args)
