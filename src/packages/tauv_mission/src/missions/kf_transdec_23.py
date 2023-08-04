6b1832f8828509d4f63e7671fd49d23ee81ef2ac
kn1451j <katerinaann@gmail.com>
2023-08-04 02:41:24 -0700

    missionnn

--- src/packages/tauv_mission/src/missions/kf_transdec_23.py
+++ src/packages/tauv_mission/src/missions/kf_transdec_23.py
@@ -9,16 +9,12 @@ import tasks.dive
 class KFTransdec23(Mission):
     def __init__(self, params : Dict[str, Union[int, float]]):
         self._params : Dict[Union[int, float]] = params
-        self._world_t_initial : Optional[SE3] = None
 
     def entrypoint(self) -> Optional[Task]:
-        return tasks.start.GetPose()
+        return tasks.dive.Dive(1.0)
 
     def transition(self, task: Task, task_result: TaskResult) -> Optional[Task]:
-        if isinstance(task, tasks.start.GetPose) and task_result.status == tasks.start.GetPoseStatus.SUCCESS:
-            self._world_t_initial = task_result.pose
-            return tasks.dive.Dive(1.0)
-        elif isinstance(task, tasks.dive.Dive) and task_result.status == tasks.dive.DiveStatus.SUCCESS:
+        if isinstance(task, tasks.dive.Dive) and task_result.status == tasks.dive.DiveStatus.SUCCESS:
             return tasks.find.Find("gate", self._params["find"])
         else:
             return None
