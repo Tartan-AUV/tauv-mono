5dbdec965be12f845ee13e2243407fdc92ebe4d2
a26blass <ablass@andrew.cmu.edu>
2023-08-05 22:26:56 -0400

    oakd error handle

--- src/packages/tauv_vehicle/src/oakd/oakd.py
+++ src/packages/tauv_vehicle/src/oakd/oakd.py
@@ -12,101 +12,7 @@ from cv_bridge import CvBridge, CvBridgeError
 class OAKDNode:
     def __init__(self, configured=False):
         try:
-            self._load_config()
-
-            self._pipeline = depthai.Pipeline()
-
-            self._color = self._pipeline.create(depthai.node.ColorCamera)
-            self._depth = self._pipeline.create(depthai.node.StereoDepth)
-            self._left = self._pipeline.create(depthai.node.MonoCamera)
-            self._right = self._pipeline.create(depthai.node.MonoCamera)
-
-            self._left.setBoardSocket(depthai.CameraBoardSocket.LEFT)
-            self._right.setBoardSocket(depthai.CameraBoardSocket.RIGHT)
-
-            self._xout_color = self._pipeline.create(depthai.node.XLinkOut)
-            self._xout_depth = self._pipeline.create(depthai.node.XLinkOut)
-
-            self._xout_color.setStreamName('rgb')
-            self._xout_depth.setStreamName('depth')
-
-            self._left.setResolution(depthai.MonoCameraProperties.SensorResolution.THE_720_P)
-            self._left.setBoardSocket(depthai.CameraBoardSocket.LEFT)
-            self._left.setFps(self._fps)
-
-            self._right.setResolution(depthai.MonoCameraProperties.SensorResolution.THE_720_P)
-            self._right.setBoardSocket(depthai.CameraBoardSocket.RIGHT)
-            self._right.setFps(self._fps)
-
-            self._color.setBoardSocket(depthai.CameraBoardSocket.RGB)
-            self._color.setResolution(depthai.ColorCameraProperties.SensorResolution.THE_1080_P)
-            self._color.setInterleaved(False)
-            self._color.setColorOrder(depthai.ColorCameraProperties.ColorOrder.RGB)
-            self._color.setFps(self._fps)
-
-            self._color_manip = self._pipeline.create(depthai.node.ImageManip)
-            self._color_manip.setResize(1280, 720)
-            self._color_manip.setMaxOutputFrameSize(1280 * 720 * 3)
-
-            self._depth.setLeftRightCheck(False)
-            self._depth.setExtendedDisparity(False)
-            self._depth.setSubpixel(False)
-            self._depth.setOutputSize(1280, 720)
-            self._depth.setDepthAlign(depthai.CameraBoardSocket.RGB)
-            self._depth.setOutputSize(1280, 720)
-            self._depth.setDefaultProfilePreset(depthai.node.StereoDepth.PresetMode.HIGH_DENSITY)
-            self._depth.initialConfig.setMedianFilter(depthai.MedianFilter.KERNEL_7x7)
-
-            if self._postprocess_depth:
-                config = self._depth.initialConfig.get()
-                config.postProcessing.speckleFilter.enable = True
-                config.postProcessing.speckleFilter.speckleRange = 50
-                config.postProcessing.temporalFilter.enable = True
-                config.postProcessing.spatialFilter.enable = True
-                config.postProcessing.spatialFilter.holeFillingRadius = 2
-                config.postProcessing.spatialFilter.numIterations = 1
-                config.postProcessing.thresholdFilter.minRange = 400
-                config.postProcessing.thresholdFilter.maxRange = 15000
-                config.postProcessing.decimationFilter.decimationFactor = 1
-                self._depth.initialConfig.set(config)
-
-            self._left.out.link(self._depth.left)
-            self._right.out.link(self._depth.right)
-            self._depth.depth.link(self._xout_depth.input)
-            self._color.isp.link(self._color_manip.inputImage)
-            self._color_manip.out.link(self._xout_color.input)
-
-            self._device = None
-            while self._device is None and not rospy.is_shutdown():
-                try:
-                    device_info = depthai.DeviceInfo(self._id)
-
-                    self._device = depthai.Device(self._pipeline, device_info)
-                except Exception as e:
-                    rospy.logerr(f'OAKD device error: {e}')
-                    rospy.sleep(1.0)
-            
-            self._calibration = self._device.readCalibration()
-            self._depth_info = CameraInfo()
-            self._depth_info.K = np.ndarray.flatten(np.array(self._calibration.getCameraIntrinsics(depthai.CameraBoardSocket.LEFT)))
-            self._depth_info.distortion_model = 'rational_polynomial'
-            self._depth_info.D = np.array(self._calibration.getDistortionCoefficients(depthai.CameraBoardSocket.LEFT))
-            self._color_info = CameraInfo()
-            self._color_info.K = np.ndarray.flatten(np.array(self._calibration.getCameraIntrinsics(depthai.CameraBoardSocket.RGB, resizeWidth=1280, resizeHeight=720)))
-            self._color_info.distortion_model = 'rational_polynomial'
-            self._color_info.D = np.array(self._calibration.getDistortionCoefficients(depthai.CameraBoardSocket.RGB))
-
-            self._bridge = CvBridge()
-
-            #estimate of ros system time offset compared to depthai clock
-            depthai_time = depthai.Clock.now()
-            self._time_offset = rospy.Time.now() - rospy.Time.from_sec(depthai_time.total_seconds())
-            rospy.loginfo(f'Time offset: {self._time_offset}')
-
-            self._depth_pub = rospy.Publisher(f'vehicle/{self._frame}/depth/image_raw', Image, queue_size=self._queue_size)
-            self._color_pub = rospy.Publisher(f'vehicle/{self._frame}/color/image_raw', Image, queue_size=self._queue_size)
-            self._depth_info_pub = rospy.Publisher(f'vehicle/{self._frame}/depth/camera_info', CameraInfo, queue_size=1, latch=True)
-            self._color_info_pub = rospy.Publisher(f'vehicle/{self._frame}/color/camera_info', CameraInfo, queue_size=1, latch=True)
+            self._setup()
         except Exception as e:
             rospy.logerr('init failed (retrying): {e}')
             if not rospy.is_shutdown:
@@ -159,6 +65,103 @@ class OAKDNode:
                     self._depth_pub.publish(img)
                 except CvBridgeError as e:
                     rospy.loginfo(f'OAKD frame error: {e}')
+    
+    def _setup(self):
+        self._load_config()
+
+        self._pipeline = depthai.Pipeline()
+
+        self._color = self._pipeline.create(depthai.node.ColorCamera)
+        self._depth = self._pipeline.create(depthai.node.StereoDepth)
+        self._left = self._pipeline.create(depthai.node.MonoCamera)
+        self._right = self._pipeline.create(depthai.node.MonoCamera)
+
+        self._left.setBoardSocket(depthai.CameraBoardSocket.LEFT)
+        self._right.setBoardSocket(depthai.CameraBoardSocket.RIGHT)
+
+        self._xout_color = self._pipeline.create(depthai.node.XLinkOut)
+        self._xout_depth = self._pipeline.create(depthai.node.XLinkOut)
+
+        self._xout_color.setStreamName('rgb')
+        self._xout_depth.setStreamName('depth')
+
+        self._left.setResolution(depthai.MonoCameraProperties.SensorResolution.THE_720_P)
+        self._left.setBoardSocket(depthai.CameraBoardSocket.LEFT)
+        self._left.setFps(self._fps)
+
+        self._right.setResolution(depthai.MonoCameraProperties.SensorResolution.THE_720_P)
+        self._right.setBoardSocket(depthai.CameraBoardSocket.RIGHT)
+        self._right.setFps(self._fps)
+
+        self._color.setBoardSocket(depthai.CameraBoardSocket.RGB)
+        self._color.setResolution(depthai.ColorCameraProperties.SensorResolution.THE_1080_P)
+        self._color.setInterleaved(False)
+        self._color.setColorOrder(depthai.ColorCameraProperties.ColorOrder.RGB)
+        self._color.setFps(self._fps)
+
+        self._color_manip = self._pipeline.create(depthai.node.ImageManip)
+        self._color_manip.setResize(1280, 720)
+        self._color_manip.setMaxOutputFrameSize(1280 * 720 * 3)
+
+        self._depth.setLeftRightCheck(False)
+        self._depth.setExtendedDisparity(False)
+        self._depth.setSubpixel(False)
+        self._depth.setOutputSize(1280, 720)
+        self._depth.setDepthAlign(depthai.CameraBoardSocket.RGB)
+        self._depth.setOutputSize(1280, 720)
+        self._depth.setDefaultProfilePreset(depthai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+        self._depth.initialConfig.setMedianFilter(depthai.MedianFilter.KERNEL_7x7)
+
+        if self._postprocess_depth:
+            config = self._depth.initialConfig.get()
+            config.postProcessing.speckleFilter.enable = True
+            config.postProcessing.speckleFilter.speckleRange = 50
+            config.postProcessing.temporalFilter.enable = True
+            config.postProcessing.spatialFilter.enable = True
+            config.postProcessing.spatialFilter.holeFillingRadius = 2
+            config.postProcessing.spatialFilter.numIterations = 1
+            config.postProcessing.thresholdFilter.minRange = 400
+            config.postProcessing.thresholdFilter.maxRange = 15000
+            config.postProcessing.decimationFilter.decimationFactor = 1
+            self._depth.initialConfig.set(config)
+
+        self._left.out.link(self._depth.left)
+        self._right.out.link(self._depth.right)
+        self._depth.depth.link(self._xout_depth.input)
+        self._color.isp.link(self._color_manip.inputImage)
+        self._color_manip.out.link(self._xout_color.input)
+
+        self._device = None
+        while self._device is None and not rospy.is_shutdown():
+            try:
+                device_info = depthai.DeviceInfo(self._id)
+
+                self._device = depthai.Device(self._pipeline, device_info)
+            except Exception as e:
+                rospy.logerr(f'OAKD device error: {e}')
+                rospy.sleep(1.0)
+        
+        self._calibration = self._device.readCalibration()
+        self._depth_info = CameraInfo()
+        self._depth_info.K = np.ndarray.flatten(np.array(self._calibration.getCameraIntrinsics(depthai.CameraBoardSocket.LEFT)))
+        self._depth_info.distortion_model = 'rational_polynomial'
+        self._depth_info.D = np.array(self._calibration.getDistortionCoefficients(depthai.CameraBoardSocket.LEFT))
+        self._color_info = CameraInfo()
+        self._color_info.K = np.ndarray.flatten(np.array(self._calibration.getCameraIntrinsics(depthai.CameraBoardSocket.RGB, resizeWidth=1280, resizeHeight=720)))
+        self._color_info.distortion_model = 'rational_polynomial'
+        self._color_info.D = np.array(self._calibration.getDistortionCoefficients(depthai.CameraBoardSocket.RGB))
+
+        self._bridge = CvBridge()
+
+        #estimate of ros system time offset compared to depthai clock
+        depthai_time = depthai.Clock.now()
+        self._time_offset = rospy.Time.now() - rospy.Time.from_sec(depthai_time.total_seconds())
+        rospy.loginfo(f'Time offset: {self._time_offset}')
+
+        self._depth_pub = rospy.Publisher(f'vehicle/{self._frame}/depth/image_raw', Image, queue_size=self._queue_size)
+        self._color_pub = rospy.Publisher(f'vehicle/{self._frame}/color/image_raw', Image, queue_size=self._queue_size)
+        self._depth_info_pub = rospy.Publisher(f'vehicle/{self._frame}/depth/camera_info', CameraInfo, queue_size=1, latch=True)
+        self._color_info_pub = rospy.Publisher(f'vehicle/{self._frame}/color/camera_info', CameraInfo, queue_size=1, latch=True)
 
     def _load_config(self):
         self._tf_namespace = rospy.get_param('tf_namespace')
