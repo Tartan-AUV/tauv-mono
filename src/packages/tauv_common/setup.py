dd6ada761c8a70385b469862630c1356f79aff71
Theo Chemel <theo@chemel.net>
2023-10-27 18:18:14 -0400

    Showing people how 2 git

--- src/packages/tauv_common/setup.py
+++ src/packages/tauv_common/setup.py
@@ -22,6 +22,7 @@ from setuptools import find_packages
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=[
+              'accumulator',
               'thruster_managers.models',
               'controllers',
               'controllers.controller',
@@ -50,7 +51,8 @@ setup_args = generate_distutils_setup(
     # packages=find_packages(),
     package_dir={'': 'src'},
     requires=['rospy'],
-    scripts=['scripts/thruster_allocator',
+    scripts=['scripts/accumulator',
+             'scripts/thruster_allocator',
              'scripts/keyboard_planner',
              'scripts/teleop_planner',
              'scripts/controller',
