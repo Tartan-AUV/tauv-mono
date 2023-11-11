e11c00cea1dd19e58a6a2de77dcda10ca0f873ff
Niaolan27 <jasonniow.work@gmail.com>
2023-11-11 17:12:00 -0500

    intro-task:created node which filters reddish pixels and publishes to new topic

--- src/packages/tauv_common/setup.py
+++ src/packages/tauv_common/setup.py
@@ -46,7 +46,8 @@ setup_args = generate_distutils_setup(
               'motion_client',
               'tauv_messages',
               'trajectories',
-              'albatross_state_estimation'],
+              'albatross_state_estimation',
+              'intro_task'],
     # packages=find_packages(),
     package_dir={'': 'src'},
     requires=['rospy'],
@@ -68,7 +69,8 @@ setup_args = generate_distutils_setup(
              'scripts/darknet_transformer',
              'scripts/bucket_to_tf',
              'scripts/shape_detector',
-             'scripts/albatross_state_estimation'],
+             'scripts/albatross_state_estimation',
+             'scripts/intro_task'],
 
 )
 setup(**setup_args)
