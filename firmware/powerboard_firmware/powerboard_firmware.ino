a9b6ffe2474bdd67f66c79aa7e27d2f118574ac5
Kerem Doruk Korkmaz <keremdkorkmaz@gmail.com>
2024-02-10 16:25:39 -0500

    Finished testing code for arduino Serial

--- firmware/powerboard_firmware/powerboard_firmware.ino
+++ firmware/powerboard_firmware/powerboard_firmware.ino
@@ -1,10 +1,13 @@
 size_t byteWritten = 0; // for incoming serial data
-
-// uint8_t all[11] = {0, 0, 0, 0, 0, 0, 0, 255, 25};
-uint8_t all[11] = {0, 0, 0, 0, 0, 0, 1, 255, 25};
 int voltage_pin = A0;
+uint8_t buf[11];
 
 typedef enum { 
+  TEST_CODE = 0x01,
+  HEARTBEAT = 0xFF,
+  SET_POWER = 0x88,
+  THRUSTER_POWER = 0x44,
+  THRUSTER_POWER_V = 0x45,
   GET_VOLTAGE = 0x22
 } CommandType;
 
@@ -13,46 +16,58 @@ void setup() {
 }
 
 struct RequestType {
-  char command_id;
+  uint8_t command_id;
   union {
-    // payload
+    //payload
   };
+  uint16_t sumHash; 
 };
 
 struct ResponseType {
-  char command_id;
+  uint8_t command_id;
   union {
     int voltage;
   };
+  uint16_t sumHash;
 };
 
 void loop() {
   // send data only when you receive data:
   if (Serial.available() > 0) {
     // read the incoming byte:
-    struct RequestType testing;
-    uint8_t encoded_request[11];
-    memcpy(encoded_request, &GET_VOLTAGE, sizeof(CommandType));
-    memcpy(encoded_request[9], &hash(encoded_request), sizeof(uint16_t));
+    // struct RequestType testing;
+    //uint8_t encoded_request[11];
+    // memcpy(encoded_request, &GET_VOLTAGE, sizeof(CommandType));
+    // memcpy(encoded_request[9], &hash(encoded_request), sizeof(uint16_t));
 
-    testing = parse(encoded_request);
-    Serial.println(testing.command_id, DEC);
+    // testing = parse(encoded_request);
+    // Serial.println(testing.command_id, DEC);
 
-    uint8_t *encoded_response = createResponse(testing);
+    // uint8_t *encoded_response = createResponse(testing);
 
     // encoded_request[0] = GET_VOLTAGE;
     
-
-    char buf[11];
-    byteWritten = Serial.readBytes(buf, 11);
-    uint16_t test = hash(all);
+    //byteWritten = Serial.readBytes(buf, 11);
+    //uint16_t test = hash(all);
 
     // say what you got:
-    Serial.print("I received: ");
-    Serial.println(byteWritten, DEC);
-    Serial.println((char *)all);
-    printBytes(all, 11);
-    Serial.println(test, DEC);
+    //Serial.print("I received: ");
+    //Serial.println(byteWritten, DEC);
+    //Serial.println((char *)all);
+    //printBytes(all, 11);
+    //Serial.println(test, DEC);
+
+    byteWritten = Serial.read();
+    
+    memcpy(buf, &buf[1], 10 * sizeof(uint8_t));
+    buf[10] = (uint8_t) byteWritten;
+
+    if (checkSum(buf)) {
+      struct RequestType *parsedRequest = parse(buf);
+      uint8_t *response = createResponse(*parsedRequest);
+
+      Serial.write(response, 11);
+    }
   }
 }
 
@@ -69,6 +84,10 @@ uint16_t hash(uint8_t buf[9]) {
     powfactor = (powfactor * 256) % 65521;
   }
   return checksum;
+}
+
+bool checkSum(uint8_t buf[11]) {
+  return hash(buf) == *((uint16_t *)(&buf[9]));
 }
 
 struct RequestType *parse(uint8_t buf[11]) {
@@ -107,7 +126,7 @@ uint8_t *createResponse(struct RequestType request) {
       response.command_id = GET_VOLTAGE;
       response.voltage = analogRead(voltage_pin);
 
-      memcpy(response_array, &response, sizeof(response));
+      memcpy(response_array, &response,9);
       checksum = hash(response_array);
       memcpy(response_array + 9, &checksum, sizeof(uint16_t));
     }
