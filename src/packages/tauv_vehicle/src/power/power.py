630ffed2973e5beaad73ba3b2bf4cf05a3497724
LawrenceFeng17 <lawrence.feng2017@gmail.com>
2024-02-10 19:06:46 -0500

    fix

--- src/packages/tauv_vehicle/src/power/power.py
+++ src/packages/tauv_vehicle/src/power/power.py
@@ -24,7 +24,7 @@ class Power:
             checksum_bytes = struct.pack('H', checksum)
             bytes = bytes + checksum_bytes
 
-            print(bytes)
+            print(bytes.decode('hex'))
 
             ser.write(bytes)
 
