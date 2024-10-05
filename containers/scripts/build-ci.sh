c6216e49d988e1f07734add3c3688ff334edd7bc
Gleb Ryabtsev <gryabtse@andrew.cmu.edu>
2024-10-05 13:49:14 -0400

    asdf

--- containers/scripts/build-ci.sh
+++ containers/scripts/build-ci.sh
@@ -14,7 +14,7 @@ cd $REPO_ROOT/containers
 COMMIT_HASH=$(git rev-parse --short HEAD)
 
 docker build --file platform/Dockerfile.x86-nvidia --tag "tauv/x86-nvidia-platform:$COMMIT_HASH" .
-docker build --build-arg BASE_IMAGE=tauv/x86-nvidia-platform --file common/Dockerfile.common --tag "tauv/x86-nvidia-common:$COMMIT_HASH" .
-docker build --build-arg BASE_IMAGE=tauv/x86-nvidia-common --file apps/surface/Dockerfile.ci --tag "tauv/x86-nvidia-ci:$COMMIT_HASH" .
+docker build --build-arg BASE_IMAGE="tauv/x86-nvidia-platform:$COMMIT_HASH" --file common/Dockerfile.common --tag "tauv/x86-nvidia-common:$COMMIT_HASH" .
+docker build --build-arg BASE_IMAGE="tauv/x86-nvidia-common:$COMMIT_HASH" --file apps/surface/Dockerfile.ci --tag "tauv/x86-nvidia-ci:$COMMIT_HASH" .
 
 echo -e "\n\n\nFinished building the CI image: $COMMIT_HASH"
