c092652d103926e1b1d1eb22d6bd2c2b57d56291
Gleb Ryabtsev <gryabtse@andrew.cmu.edu>
2024-10-05 13:22:13 -0400

    tuff

--- containers/scripts/build-ci.sh
+++ containers/scripts/build-ci.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-cd ..
-docker build --file platform/Dockerfile.x86-nvidia --tag tauv/x86-nvidia-platform .
-docker build --build-arg BASE_IMAGE=tauv/x86-nvidia-platform --file common/Dockerfile.common --tag tauv/x86-nvidia-common .
-docker build --build-arg BASE_IMAGE=tauv/x86-nvidia-common --file apps/surface/Dockerfile.ci --tag tauv/x86-nvidia-ci .
+if [ -n "$GITHUB_WORKSPACE" ]; then
+  # If inside GitHub Actions, use GITHUB_WORKSPACE as the repo root
+  REPO_ROOT="$GITHUB_WORKSPACE"
+else
+  REPO_ROOT=$(pwd)
+fi
 
-echo -e "\n\n\nFinished building the workstation image."
+cd $REPO_ROOT/containers
+
+COMMIT_HASH=$(git rev-parse --short HEAD)
+
+docker build --file platform/Dockerfile.x86-nvidia --tag "tauv/x86-nvidia-platform:$COMMIT_HASH" .
+docker build --build-arg BASE_IMAGE=tauv/x86-nvidia-platform --file common/Dockerfile.common --tag "tauv/x86-nvidia-common:$COMMIT_HASH" .
+docker build --build-arg BASE_IMAGE=tauv/x86-nvidia-common --file apps/surface/Dockerfile.ci --tag "tauv/x86-nvidia-ci:$COMMIT_HASH" .
+
+echo -e "\n\n\nFinished building the CI image: $COMMIT_HASH"
--- containers/scripts/build-workstation.sh
+++ containers/scripts/build-workstation.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-cd ..
+if [ -n "$GITHUB_WORKSPACE" ]; then
+  # If inside GitHub Actions, use GITHUB_WORKSPACE as the repo root
+  REPO_ROOT="$GITHUB_WORKSPACE"
+else
+  REPO_ROOT=$(pwd)
+fi
+
+cd $REPO_ROOT/containers
+
 docker build --file platform/Dockerfile.x86-nvidia-turbovnc --tag tauv/x86-nvidia-turbovnc-platform .
 docker build --build-arg BASE_IMAGE=tauv/x86-nvidia-turbovnc-platform --file common/Dockerfile.common --tag tauv/x86-nvidia-common .
 docker build --build-arg BASE_IMAGE=tauv/x86-nvidia-common --file apps/surface/Dockerfile.workstation --tag tauv/x86-nvidia-workstation .
