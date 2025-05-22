91139a50e37cac65b07232a725c7f2e498cb386f
Gleb Ryabtsev <glebryabtsev@cmu.edu>
2025-05-21 23:19:19 -0400

    stuff

---
+++ packages/tauv_sim/include/tauv_sim/sim_adapter.hpp
@@ -0,0 +1,19 @@
+//
+// Created by gleb on 5/21/25.
+//
+
+#ifndef SIM_ADAPTER_H
+#define SIM_ADAPTER_H
+
+#include "rclcpp/node.hpp"
+
+using namespace rclcpp;
+
+class SimAdapter final : Node {
+public:
+  SimAdapter();
+};
+
+
+
+#endif //SIM_ADAPTER_H
---
+++ packages/tauv_sim/src/tauv_sim/sim_adapter.cpp
@@ -0,0 +1,9 @@
+//
+// Created by gleb on 5/21/25.
+//
+
+#include "tauv_sim/sim_adapter.hpp"
+
+SimAdapter::SimAdapter() : Node("sim_adapter") {
+
+}
