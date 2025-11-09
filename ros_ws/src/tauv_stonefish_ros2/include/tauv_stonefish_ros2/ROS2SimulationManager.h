38909b83992693393302e34c97bbf4d0502c2a3a
fundudeone <nicolasfrazier24@gmail.com>
2025-11-09 18:04:41 +0000

    Added static obstacles (with geometry loaded from external files) to the sim. Also made CMake output compile commands.
    Finished loading static objects from YAML (might break into more functions later)
    
    Cleaned up static objects loader

--- ros_ws/src/tauv_stonefish_ros2/include/tauv_stonefish_ros2/ROS2SimulationManager.h
+++ ros_ws/src/tauv_stonefish_ros2/include/tauv_stonefish_ros2/ROS2SimulationManager.h
@@ -1,9 +1,51 @@
 #include <Stonefish/core/SimulationManager.h>
+#include <Stonefish/entities/solids/Sphere.h>
+#include <Stonefish/entities/statics/Obstacle.h>
+#include <Stonefish/entities/statics/Plane.h>
+
+#include <filesystem>
+
+#include "yaml-cpp/yaml.h"
 
 namespace sf {
 class ROS2SimulationManager : public SimulationManager {
  public:
   ROS2SimulationManager(double stepsPerSecond);
   void BuildScenario();
+
+ private:
+  void LoadStaticObjectsFromFile(std::filesystem::path path);
+
+  // YAML parsing helper
+  template <typename T>
+  T GetNodeParameter(YAML::Node object, std::string parameterName, T defaultValue) {
+    if (object[parameterName]) {
+      return object[parameterName].as<T>();
+    }
+    return defaultValue;
+  }
 };
 }  // namespace sf
+
+// Needed for yaml-cpp's ability to load and serialize stonefish Vector3's
+namespace YAML {
+template <>
+struct convert<btVector3> {
+  static Node encode(const btVector3& rhs) {
+    Node node;
+    node.push_back(rhs.m_floats[0]);
+    node.push_back(rhs.m_floats[1]);
+    node.push_back(rhs.m_floats[2]);
+    return node;
+  }
+
+  static bool decode(const Node& node, btVector3& rhs) {
+    if (!node.IsSequence() || node.size() != 3) return false;
+
+    rhs.m_floats[0] = node[0].as<float>();
+    rhs.m_floats[1] = node[1].as<float>();
+    rhs.m_floats[2] = node[2].as<float>();
+    return true;
+  }
+};
+}  // namespace YAML
--- ros_ws/src/tauv_stonefish_ros2/src/tauv_stonefish_ros2/ROS2SimulationManager.cpp
+++ ros_ws/src/tauv_stonefish_ros2/src/tauv_stonefish_ros2/ROS2SimulationManager.cpp
@@ -1,7 +1,12 @@
 #include "tauv_stonefish_ros2/ROS2SimulationManager.h"
 
 #include <Stonefish/entities/solids/Sphere.h>
+#include <Stonefish/entities/statics/Obstacle.h>
 #include <Stonefish/entities/statics/Plane.h>
+
+#include <filesystem>
+
+#include "yaml-cpp/yaml.h"
 
 namespace sf {
 ROS2SimulationManager::ROS2SimulationManager(double stepsPerSecond)
@@ -26,5 +31,49 @@ void ROS2SimulationManager::BuildScenario() {
   // Create object
   Sphere* sph = new Sphere("Sphere", BodyPhysicsSettings(), 0.1, I4(), "Aluminium", "red");
   AddSolidEntity(sph, Transform(IQ(), Vector3(0.0, 0.0, -1.0)));
+
+  // Create static object
+  LoadStaticObjectsFromFile("./src/tauv_stonefish_ros2/data/staticObjects.yaml");
 }
+
+void ROS2SimulationManager::LoadStaticObjectsFromFile(std::filesystem::path path) {
+  YAML::Node staticObjectsParser = YAML::LoadFile(path);
+
+  for (auto it = staticObjectsParser.begin(); it != staticObjectsParser.end(); it++) {
+    YAML::Node object = it->second;
+
+    // object["field"] checks if object contains the key "field" if used in a conditonal
+    if (!object["MeshPath"]) {
+      printf("Object specified in static object file is missing a mesh!\n");
+      continue;
+    }
+
+    std::string meshPath = GetNodeParameter<std::string>(object, "MeshPath", "");
+
+    // if the path is local, join it to the directory of the yaml file we're reading
+    if (meshPath[0] == '.') {
+      std::filesystem::path parent = path.parent_path();
+      parent.append(meshPath);
+      meshPath = parent;
+    }
+
+    Vector3 translation = GetNodeParameter<Vector3>(object, "Translation", Vector3(0, 0, 0));
+
+    Quaternion rotation = IQ();
+    Vector3 euler = GetNodeParameter<Vector3>(object, "EulerAngles", Vector3(0, 0, 0));
+    rotation.setEulerZYX(euler.z(), euler.y(), euler.x());
+
+    float scale = GetNodeParameter<float>(object, "Scale", 1);
+
+    Transform objectTransform = Transform(rotation, translation);
+
+    // it->first contains the "key"/name of the current Node
+    Obstacle* staticObject =
+        new Obstacle(it->first.as<std::string>().c_str(), meshPath, scale, objectTransform,
+                     meshPath, scale, objectTransform, false, "Steel", "gray");
+
+    AddStaticEntity(staticObject, I4());
+  }
+}
+
 }  // namespace sf
